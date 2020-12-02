d40f6d900d48c1304306fb2f569f40ff60a3b06e
Rory Collins <rory.collins@hotmail.co.uk>
2020-12-02 08:45:50 +0000

    restructure codebase

---
+++ DayOne/ReportRepair.py
@@ -0,0 +1,21 @@
+import itertools as it
+from functools import reduce
+from operator import mul
+
+
+def product_of_records(number_of_records, records):
+    all_combinations = list(it.combinations(records, number_of_records))
+    combination = next(x for x in all_combinations if sum(x) == 2020)
+    return reduce(mul, combination, 1)
+
+
+def read_expense_records():
+    f = open("expenseReport.txt")
+    records = list(map(int, f.read().splitlines()))
+    f.close()
+    return records
+
+
+expense_records = read_expense_records()
+print("Part one:", product_of_records(2, expense_records))
+print("Part two:", product_of_records(3, expense_records))
