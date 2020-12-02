380254dd3c47ce5b7f73cb2f1535efb105ccbf36
Rory Collins <rory.collins@hotmail.co.uk>
2020-12-02 11:21:43 +0000

    tidy up

--- DayTwo/PasswordPhilosophy.py
+++ DayTwo/PasswordPhilosophy.py
@@ -3,16 +3,11 @@ from DayTwo.Password import Password
 
 def read_passwords():
     f = open("passwords.txt")
-    passwords = f.read().splitlines()
+    password_records = f.read().splitlines()
     f.close()
-    return passwords
+    return list(map(Password, password_records))
 
 
-def main():
-    passwords = list(map(Password, read_passwords()))
-    print("Part one: ", sum(x.validate_part_one() for x in passwords))
-    print("Part two: ", sum(x.validate_part_two() for x in passwords))
-
-
-if __name__ == "__main__":
-    main()
+passwords = read_passwords()
+print("Part one: ", sum(x.validate_part_one() for x in passwords))
+print("Part two: ", sum(x.validate_part_two() for x in passwords))
