6567ea193fe9479d3db01545b75adf7f6a017911
Rory Collins <rory.collins@hotmail.co.uk>
2020-12-03 11:55:07 +0000

    use map/reduce for part two

--- DayThree/TobogganTrajectory.py
+++ DayThree/TobogganTrajectory.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from operator import mul
+
 f = open("geography.txt")
 lines = f.read().splitlines()
 f.close()
@@ -16,11 +19,9 @@ def recursive_check_slope(right, down, x, y, trees):
     return recursive_check_slope(right, down, (x + right) % width, y + down, total_trees_hit)
 
 
-route_one = check_slope(1, 1)
-route_two = check_slope(3, 1)
-route_three = check_slope(5, 1)
-route_four = check_slope(7, 1)
-route_five = check_slope(1, 2)
+print("Part one:", check_slope(3, 1))
 
-print("Part one:", route_two)
-print("Part two:", route_one * route_two * route_three * route_four * route_five)
+all_slopes = (1, 1), (3, 1), (5, 1), (7, 1), (1, 2)
+trees_in_slopes = map(lambda rd: check_slope(rd[0], rd[1]), all_slopes)
+product_of_trees_in_slopes = reduce(mul, trees_in_slopes, 1)
+print("Part two:", product_of_trees_in_slopes)
