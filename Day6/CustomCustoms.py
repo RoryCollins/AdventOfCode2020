b4b8aa99396444dacd333bc9b13e0dd85a22fe23
Rory Collins <rory.collins@hotmail.co.uk>
2020-12-06 13:42:29 +0000

    Add solution for Day6 part two

--- Day6/CustomCustoms.py
+++ Day6/CustomCustoms.py
@@ -1,8 +1,18 @@
 import re
 
-f = open("customs_responses.txt", "r")
-group_responses = map(lambda x: x.replace("\n", ""), list(re.split("\n\n", f.read())))
-f.close()
 
-print("Part one:", sum(list(map(lambda x: len(''.join(set(x))), group_responses))))
+def read_responses():
+    f = open("customs_responses.txt", "r")
+    group_responses = list(re.split("\n\n", f.read()))
+    f.close()
+    return group_responses
 
+
+print("Part one:", sum(list(map(lambda x: len(''.join(set(x))), map(lambda x: x.replace("\n", ""), read_responses())))))
+
+
+def count_common_answers(group_response):
+    return len([x for x in filter(lambda x: all(x in response for response in group_response), group_response[0])])
+
+
+print("Part two:", sum(map(count_common_answers, map(lambda x: re.split("\n", x), read_responses()))))
