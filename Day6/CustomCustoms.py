10c01debb2f610d2e8cbddf33cff160db150a085
Rory Collins <rory.collins@hotmail.co.uk>
2020-12-07 09:22:37 +0000

    tidy up day 6

--- Day6/CustomCustoms.py
+++ Day6/CustomCustoms.py
@@ -3,16 +3,20 @@ import re
 
 def read_responses():
     f = open("customs_responses.txt", "r")
-    group_responses = list(re.split("\n\n", f.read()))
+    group_responses = map(lambda x: re.split("\n", x), list(re.split("\n\n", f.read())))
     f.close()
     return group_responses
 
 
-print("Part one:", sum(list(map(lambda x: len(''.join(set(x))), map(lambda x: x.replace("\n", ""), read_responses())))))
+def concatenated_responses(responses):
+    return map(lambda x: ''.join(x), responses)
+
+
+print("Part one:", sum(map(lambda x: len(''.join(set(x))), concatenated_responses(read_responses()))))
 
 
 def count_common_answers(group_response):
     return len([x for x in filter(lambda x: all(x in response for response in group_response), group_response[0])])
 
 
-print("Part two:", sum(map(count_common_answers, map(lambda x: re.split("\n", x), read_responses()))))
+print("Part two:", sum(map(count_common_answers, read_responses())))
